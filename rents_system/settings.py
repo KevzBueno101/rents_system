89b8ebea9a01db296bc7ab669059ded877dbecfc
Kevin Bueno <KevzBueno101@users.noreply.github.com>
2026-04-21 13:08:47 +0800

    configure settings for deployment

--- rents_system/settings.py
+++ rents_system/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-^n#s%*ak9+!^i+k$4-c(%xv&dnusxt-a@x@=mx^_ssw49(dunq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
