9fede4b8feb00788e072ec618ae3c08292bc7050
Kevin Bueno <KevzBueno101@users.noreply.github.com>
2026-04-30 21:29:48 +0800

    fix: Add favicon URL pattern to prevent 404 errors on deployed app
    - Add favicon.ico URL pattern using RedirectView
    - Fix typo in HttpResponse import in admin_views.py
    - Create empty favicon.ico file in static directory
    - Prevent 404 errors when browsers request favicon.ico

--- rents_system/urls.py
+++ rents_system/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    # Favicon fix for deployed apps
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
